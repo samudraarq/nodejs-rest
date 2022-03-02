15728acd7e7e70414cf8c390966f86ee64d28305
Samudra Faris Arqam <samudrafaris@gmail.com>
2022-03-02 09:43:39 +0700

    install and using socket io

--- app.js
+++ app.js
@@ -65,6 +65,10 @@ mongoose
     `mongodb+srv://admin-samudra:${process.env.MONGODB_PASS}@cluster0.cd33k.mongodb.net/messages?retryWrites=true&w=majority`
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
